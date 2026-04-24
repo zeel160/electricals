d91fe3683b58daf6747dae076e4870751702aed8
zeel1607 <zeelhr24@gmail.com>
2026-04-24 08:46:49 +0530

    Update map location

--- client/src/pages/ContactPage.js
+++ client/src/pages/ContactPage.js
@@ -59,7 +59,7 @@ export default function ContactPage() {
           <div className="map-embed">
             <iframe
               title="Anchal Electricals Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.0!2d72.6419!3d23.0038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDAwJzEzLjciTiA3MsKwMzgnMzAuOCJF!5e0!3m2!1sen!2sin!4v1234567890"
+              src="https://maps.app.goo.gl/F5aeYY2RSw5Rts6Y6"
               width="100%" height="250" style={{ border: 0, borderRadius: 12 }} allowFullScreen loading="lazy"
             />
           </div>
